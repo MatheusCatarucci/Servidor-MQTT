65a2b0df812d0e02d927c694acd3b763f4fa18c5
Matheus Catarucci <matheus.catarucci7@gmail.com>
2026-05-28 09:13:51 -0300

    Servidor e API reajustados

--- app.py
+++ app.py
@@ -2,23 +2,25 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from server import *
 
 app = FastAPI()
-
+iniciar()
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
 
 #-----------------------------------------------------
 
-
 @app.get('/', response_class=HTMLResponse)
 def home(request: Request):    
-    status = {
-        "dispositivo": "off"
-    }
     return templates.TemplateResponse(
+
+        estado_teste = {{
+            "dispositvo":"on"
+        }}
+
         request=request,
         name='home.html',
-        context={'status': status}
+        context=estado_teste
     )
