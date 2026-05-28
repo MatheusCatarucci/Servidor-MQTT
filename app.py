648fbccd3efaba6335e154e072c3bf2eb68f2460
Matheus Catarucci <matheus.catarucci7@gmail.com>
2026-05-28 09:47:59 -0300

    Front-bugfix

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from server import *
 
 app = FastAPI()
-iniciar()
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
@@ -13,14 +11,15 @@ templates = Jinja2Templates(directory='templates')
 #-----------------------------------------------------
 
 @app.get('/', response_class=HTMLResponse)
-def home(request: Request):    
+def home(request: Request):  
+    estado_teste = {
+        "dispositivo": "on"
+    }
+
+    if estado_teste['dispositivo'] == 'on':
+        return
     return templates.TemplateResponse(
-
-        estado_teste = {{
-            "dispositvo":"on"
-        }}
-
         request=request,
         name='home.html',
-        context=estado_teste
+        context={'status': estado_teste}
     )
